ebd6467bdb32c7ea4d4155389da65854076bd293
andrew (m) <andrewcgraves@gmail.com>
2022-10-15 23:41:47 -0700

    drawing and color and thickness

--- client/src/components/Pallette.vue
+++ client/src/components/Pallette.vue
@@ -17,8 +17,9 @@ let drawState = useDrawStore()
         <button class="mr-2 px-2 rounded" :class="drawState.thickness == 2 ? ['bg-pastel-purple'] : []" @click="drawState.setThickness(2)"><div class="w-6 h-6 rounded-full bg-[#333333]"></div></button>
         <button class="mr-auto px-2 rounded" :class="drawState.thickness == 1 ? ['bg-pastel-purple'] : []" @click="drawState.setThickness(1)"><div class="w-5 h-5 rounded-full bg-[#333333]"></div></button>
 
-        <button class="ml-auto px-2 rounded" :class="drawState.color == '#333333' ? ['bg-pastel-purple'] : []" @click="drawState.setColor('#333333')"><div class="w-8 h-8 rounded-full bg-[#333333]"></div></button>
-        
+        <button class="ml-auto mr-2 px-2 rounded" :class="drawState.color == '#333333' ? ['bg-pastel-purple'] : []" @click="drawState.setColor('#333333')"><div class="w-8 h-8 rounded-full bg-[#333333]"></div></button>
+        <button class="mr-2 px-2 rounded" :class="drawState.color == '#fc0004' ? ['bg-pastel-purple'] : []" @click="drawState.setColor('#fc0004')"><div class="w-8 h-8 rounded-full bg-[#fc0004]"></div></button>
+        <button class="px-2 rounded" :class="drawState.color == '#6b4c30' ? ['bg-pastel-purple'] : []" @click="drawState.setColor('#6b4c30')"><div class="w-8 h-8 rounded-full bg-[#6b4c30]"></div></button>
     </div>
 </div>
 </template>
--- client/src/components/SquinqCanvas.vue
+++ client/src/components/SquinqCanvas.vue
@@ -1,12 +1,81 @@
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted } from 'vue';
+import { useDrawStore } from '@/stores/draw';
 import SquanqEntry from "../components/SquanqEntry.vue"
 import SquaglViewer from "../components/SquaglViewer.vue"
 
 let size = ref(0)
 let ctx: CanvasRenderingContext2D;
 
+let drawState = useDrawStore()
+
 // Drawing LMFAO
+
+window.addEventListener('load', ()=>{
+        
+  document.addEventListener('mousedown', startPainting);
+  document.addEventListener('mouseup', stopPainting);
+  document.addEventListener('mousemove', sketch);
+  // window.addEventListener('resize', resize);
+});
+
+let paint = false
+let coord = {x:0 , y:0}; 
+
+function getPosition(event: MouseEvent){
+  let canvas = document.getElementById('squanq-drawer') as HTMLCanvasElement
+
+  coord.x = event.clientX - canvas.offsetLeft;
+  coord.y = event.clientY - canvas.offsetTop;
+}
+  
+// The following functions toggle the flag to start
+// and stop drawing
+function startPainting(event: MouseEvent){
+  paint = true;
+  getPosition(event);
+}
+
+function stopPainting(){
+  paint = false;
+}
+
+function sketch(event: MouseEvent){
+  if (!paint) return;
+  ctx.beginPath();
+    
+  ctx.lineWidth = 5;
+   
+  // Sets the end of the lines drawn
+  // to a round shape.
+  ctx.lineCap = 'round';
+          
+  // The cursor to start drawing
+  // moves to this coordinate
+  ctx.moveTo(coord.x-10, coord.y-10);
+   
+  // The position of the cursor
+  // gets updated as we move the
+  // mouse around.
+  getPosition(event);
+   
+  // A line is traced from start
+  // coordinate to this coordinate
+  if (drawState.tool == 'brush') {
+    ctx.lineWidth = drawState.thickness
+    ctx.strokeStyle = drawState.color
+    ctx.lineTo(coord.x-10 , coord.y-10);
+      
+    // Draws the line.
+    ctx.stroke();
+  } else if (drawState.tool == 'erase') {
+    ctx.lineWidth = drawState.thickness * 3
+    ctx.strokeStyle = '#ffffff'
+    ctx.lineTo(coord.x-10 , coord.y-10);
+    // Draws the line.
+    ctx.stroke();
+  }
+}
 
 let calcWidthHeight = () => {
   if (window.innerWidth < window.innerHeight) {
@@ -35,6 +104,10 @@ onMounted(() => {
 
   ctx.fillStyle = "#ffffff"
   ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  ctx.beginPath()
+  
+
 
 })
 
